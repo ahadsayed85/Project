05a086d2cebe955c257c2f02101862f1f1d7073b
ahad sayed <ahadsayed85@gmail.com>
2023-03-13 15:38:12 +0530

    dom and bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Header from './View/Header/Header';
+import {Routes, Route} from 'react-router-dom';
+import Home from './component/Home/Home';
+import About from './component/About/About';
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="about" element={<About/>} />
+      </Routes>
     </div>
   );
 }
